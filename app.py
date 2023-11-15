347b44331ed16e06ef6113fdba13c410a009b682
Sai Prakash <63905195+SaiSpr@users.noreply.github.com>
2023-11-16 02:51:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,28 +1,55 @@
 import streamlit as st
-import numpy as np
-import ktrain
-from ktrain import text
-from transformers import BertTokenizer, BertForSequenceClassification
-import torch
+from transformers import pipeline
 
-# @st.cache(allow_output_mutation=True)
-# def get_model():
-#     tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
-#     model = BertForSequenceClassification.from_pretrained("pnichite/YTFineTuneBert")
-#     return tokenizer,model
+option = st.selectbox(
+    "Select an Option",
+    [
+        "Classify Text",
+        "Question Answering",
+        "Text Generation",
+        "Named Entity Recognition",
+        "Summarization",
+        "Translation",
+    ],
+)
 
-
-
-# Load the saved BERT model
-loaded_model = ktrain.load_predictor('path_to_saved_model')
-
-# Create a Streamlit application for text classification
-st.title('Text Classification App')
-user_input = st.text_input('Enter the text to classify')
-if st.button('Classify'):
-    prediction = loaded_model.predict(user_input)
-    st.write('The text is classified as:', prediction)
-
+if option == "Classify Text":
+    text = st.text_area(label="Enter text")
+    if text:
+        classifier = pipeline("sentiment-analysis")
+        answer = classifier(text)
+        st.write(answer)
+elif option == "Question Answering":
+    q_a = pipeline("question-answering")
+    context = st.text_area(label="Enter context")
+    question = st.text_area(label="Enter question")
+    if context and question:
+        answer = q_a({"question": question, "context": context})
+        st.write(answer)
+elif option == "Text Generation":
+    text = st.text_area(label="Enter text")
+    if text:
+        text_generator = pipeline("text-generation")
+        answer = text_generator(text)
+        st.write(answer)
+elif option == "Named Entity Recognition":
+    text = st.text_area(label="Enter text")
+    if text:
+        ner = pipeline("ner")
+        answer = ner(text)
+        st.write(answer)
+elif option == "Summarization":
+    summarizer = pipeline("summarization")
+    article = st.text_area(label="Paste Article")
+    if article:
+        summary = summarizer(article, max_length=400, min_length=30)
+        st.write(summary)
+elif option == "Translation":
+    translator = pipeline("translation_en_to_de")
+    text = st.text_area(label="Enter text")
+    if text:
+        translation = translator(text)
+        st.write(translation)
 # tokenizer,model = get_model()
 
 # user_input = st.text_area('Enter Text to Analyze')
